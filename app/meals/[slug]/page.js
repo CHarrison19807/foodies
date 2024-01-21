095e7c00b82dc6fc55b1601f9ca414c70ad17851
Connor Harrison <connorh.uottawa@gmail.com>
2024-01-21 02:09:13 -0500

    Add content and styling for individual meal page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,54 @@
-function IndividualMealPage({ params }) {
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+  const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  const { title, summary } = meal;
+
+  return {
+    title,
+    description: summary,
+  };
+}
+
+async function IndividualMealPage({ params }) {
+  const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
+
+  const { title, image, summary, instructions, creator, creator_email } = meal;
+
   return (
-    <main>
-      <h1>Individual Meal: {params.slug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={image} alt={title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={classes.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
 
